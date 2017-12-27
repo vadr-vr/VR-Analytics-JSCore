f22b07c9994a0914140e9bed0516ad5e0c191952
Abhishek Bansal <abhishekbansal1993@gmail.com>
2017-12-27 10:46:15 +0530

    Updated readme

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ import dataCollector from './js/dataCollector/dataCollector';
  * @module VadrJSCore
  * @description Provides the core functionality for vadr analytics to platform 
  * specific SDKs such as AFrame, ReactVR.
- * Checks for default events that need to be generated at each frame.
- * Makes calls to generate default events.
- * Provides unix time of each frame begin to all modules.
+ * To start using VadrAnalytics, please visit http://www.vadr.io.
+ * To integrate our SDk, look out for platform specific SDK, for AFrame and ReactVR. 
+ * If you are using any other platform for development, please reach out to abhishek@vadr.io.
  */
 logger.setLogLevel(4);
 
